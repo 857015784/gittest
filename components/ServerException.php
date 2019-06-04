eff659039525c2906fbf1699397a61a88b126c0d
wanghuabin <wanghuabin@globalegrow.com>
2019-06-04 18:08:06 +0800

    优化公共错误信息

--- components/ServerException.php
+++ components/ServerException.php
@@ -8,17 +8,55 @@
  */
 
 namespace components;
+use \ReflectionClass as ReflectionClass;
 
 class ServerException extends \Exception
 {
+    private $constants = [];
     const PARAMS_IS_ERR = 40000;
-    private static $msg = [
-        self::PARAMS_IS_ERR => ' Parameter is wrong',
-    ];
+    const PARAMS_IS_ERR_MSG = ' Parameter is wrong';
 
-    public function __construct(int $code)
+    /**
+     * 获取类中常量数组
+     * @return array $this->constants
+     */
+    protected function getConstants()
     {
-        $message = self::$msg[$code] ?? '';
-        parent::__construct($message, $code);
+        $reflection = new ReflectionClass($this);
+
+        if (0 < count($this->constants)) {
+            return $this->constants;
+        }
+
+        $this->constants = $reflection->getConstants();
+        return $this->constants;
+    }
+    /**
+     * @param  integer $code  异常编码
+     * @return string  $sMsg  异常编码消息
+     */
+    public function getMsg($code)
+    {
+        $msg    = '';
+        $msgKey = '';
+        $this->getConstants();
+
+        foreach($this->constants as $codeKey => $codeVal) {
+            if ($code == $codeVal) {
+                $msgKey = "{$codeKey}_MSG";
+                continue;
+            } else if ($msgKey == $codeKey) {
+                $msg = $codeVal;
+                break;
+            }
+        }
+
+        return $msg;
+    }
+
+    public function __construct(int $code, string $msg = '')
+    {
+        '' === $msg && $msg = $this->getMsg($code);
+        parent::__construct($msg, $code);
     }
 }
